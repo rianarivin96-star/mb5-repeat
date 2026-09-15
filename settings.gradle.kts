6e1f42d0d01609be2bf724e73378fa8b3f77cdeb
rianarivin96-star <rianarivin96@gmail.com>
2026-09-16 00:49:05 +0700

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+rootProject.name = "MB5AutoClick"
+include(":app")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
+    }
+}
